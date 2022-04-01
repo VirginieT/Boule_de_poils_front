2f704b35af32ea62df11507aefe5d20e101d3f89
anthonymayor0clock <amayor@hotmail.fr>
2022-04-01 20:12:22 +0200

    mise en place composant de la nav bar

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,11 +2,13 @@
 import reactLogo from './react-logo.svg';
 import './styles.css';
 
+import NavBar from '../NavBar';
+
 // == Composant
 const App = () => (
   <div className="app">
     <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <NavBar />
   </div>
 );
 
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,23 @@
+import "./navbar.scss";
+
+const NavBar = () => (
+  <div className="container">
+    <ul>
+      <li>logo</li>
+      <li>Boule de poils</li>
+      <li>Login</li>
+    </ul>
+    <ul className="Navbar-Menu">
+
+      <li>Mes favoris</li>
+      <li>Rechercher un animal</li>
+      <li>Conseils pour l'adoption</li>
+      <li>Les associations</li>
+      <li>Admin/ compte user</li>
+    </ul>
+  </div>
+
+
+);
+
+export default NavBar;
