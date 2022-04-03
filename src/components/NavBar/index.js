5f78522649eafda7863458bd5387e347c0472d20
anthonymayor0clock <amayor@hotmail.fr>
2022-04-03 15:34:46 +0200

    prototype barre de navigation

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,23 +1,62 @@
-import "./navbar.scss";
+import { useEffect, useState } from 'react';
+import MediaQuery from 'react-responsive';
 
-const NavBar = () => (
+import './navbar.scss';
+
+const NavBar = () => {
+  
+    const [toggleMenu, setToggleMenu] = useState(false);
+  
+    const toggleNavSmallScreen = () => {
+
+      setToggleMenu(!toggleMenu);
+    }
+  
+    useEffect(() =>{
+
+      const changeWidth = () => {
+          setLargeur(window.innerWidth);
+
+         if (window.innerWidth > 500) {
+        setToggleMenu(false);
+          }
+
+      }
+
+      window.addEventListener('resize', changeWidth);
+      return() => {
+
+          window.removeEventListener('resize', changeWidth)
+      }
+
+    }, [])
+    
+    
+
+    return(
   <div className="container">
-    <ul className="alignement">
-      <li>logo</li>
-      <li>Boule de poils</li>
-      <li>Login</li>
-    </ul>
+
+
+      <ul className="alignement">
+
+        <li>logo</li>
+        <li><h1>Boule de poils</h1></li>
+        <li>Login</li>
+      </ul>
+      {toggleMenu && (
     <ul className="Navbar-Menu">
 
-      <li>Mes favoris</li>
-      <li>Rechercher un animal</li>
-      <li>Conseils pour l'adoption</li>
-      <li>Les associations</li>
-      <li>Admin/ compte user</li>
+      <li className="items">Mes Favoris</li>
+      <li className="items">Rechercher un animal</li>
+      <li className="items">Conseils pour adoption</li>
+      <li className="items">Les associations</li>
+      <li className="items">Admin/compte user</li>
     </ul>
+    )}
+    <button onClick={ toggleNavSmallScreen} className='btn'>---</button>
+
   </div>
-
-
-);
+  );
+};
 
 export default NavBar;
