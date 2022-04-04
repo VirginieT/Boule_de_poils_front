4b8307bed1dc2d178579d8fa0cfff1ed64f6b2fb
Francois-Louis <fl.toussaint.art@gmail.com>
2022-04-04 10:30:03 +0200

    feat: mise en place du theme graphique mui + test-ok

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,33 @@
 // == Import
-import reactLogo from './react-logo.svg';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import SearchForm from '../SearchForm';
+import TestTheme from '../TestTheme';
 import './styles.css';
+
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#735F6E',
+      contrastText: '#F2F0F2',
+      light: '#8C7488',
+      dark: '#594A55',
+    },
+    secondary: {
+      main: '#D97C0B',
+      light: '#F2B33D',
+      dark: '#BF6D0A',
+      contrastText: '#F2F0F2',
+    },
+  },
+});
 
 // == Composant
 const App = () => (
   <div className="app">
-    <img src={reactLogo} alt="react logo" />
-    <h1>Composant : App</h1>
+    <ThemeProvider theme={theme}>
+      <TestTheme />
+    </ThemeProvider>
   </div>
 );
 
