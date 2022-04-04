ca5b7a1120a9e419c7a90ecd1b416077b98d129d
Francois-Louis <fl.toussaint.art@gmail.com>
2022-04-04 13:40:13 +0200

    feat: addordion done

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -12,6 +12,10 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function SignIn() {
   const [species, setSpecies] = React.useState('');
@@ -43,96 +47,106 @@ export default function SignIn() {
           alignItems: 'center',
         }}
       >
-        <Typography component="h1" variant="h5">
-          Rechercher un animal
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label" sx={{ mt: 1 }}>Espèce</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={species}
-              label="Species"
-              onChange={handleChangeSpecies}
-              sx={{ mt: 2 }}
+        <Accordion>
+          <Button variant="contained" fullWidth>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
             >
-              <MenuItem value={10}>Chien</MenuItem>
-              <MenuItem value={20}>Chat</MenuItem>
-              <MenuItem value={30}>Lapin</MenuItem>
-              <MenuItem value={40}>Rongeur</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl fullWidth>
-            <FormLabel id="demo-row-radio-buttons-group-label" sx={{ mt: 1 }}>Genre</FormLabel>
-            <RadioGroup
-              row
-              aria-labelledby="demo-row-radio-buttons-group-label"
-              name="row-radio-buttons-group"
-            >
-              <FormControlLabel value="femelle" control={<Radio />} label="Femelle" />
-              <FormControlLabel value="mâle" control={<Radio />} label="Mâle" />
-              <FormControlLabel value="both" control={<Radio />} label="Indifférent" />
-            </RadioGroup>
-          </FormControl>
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label" sx={{ mt: 1 }}>Âge</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={age}
-              label="Age"
-              onChange={handleChangeAge}
-              sx={{ mt: 2 }}
-            >
-              <MenuItem value={10}>0 à 5 ans</MenuItem>
-              <MenuItem value={20}>5 à 10 ans</MenuItem>
-              <MenuItem value={30}>10 à 15 ans</MenuItem>
-              <MenuItem value={40}>plus de 15 ans</MenuItem>
-            </Select>
-          </FormControl>
-          <FormControl fullWidth>
-            <FormLabel id="demo-row-radio-buttons-group-label" sx={{ mt: 1 }}>Compatibilité enfants</FormLabel>
-            <RadioGroup
-              row
-              aria-labelledby="demo-row-radio-buttons-group-label"
-              name="row-radio-buttons-group"
-            >
-              <FormControlLabel value="female" control={<Radio />} label="Oui" />
-              <FormControlLabel value="male" control={<Radio />} label="Non" />
-            </RadioGroup>
-          </FormControl>
-          <FormControl fullWidth>
-            <FormLabel id="demo-row-radio-buttons-group-label" sx={{ mt: 1 }}>Compatibilité autres animaux</FormLabel>
-            <RadioGroup
-              row
-              aria-labelledby="demo-row-radio-buttons-group-label"
-              name="row-radio-buttons-group"
-            >
-              <FormControlLabel value="female" control={<Radio />} label="Oui" />
-              <FormControlLabel value="male" control={<Radio />} label="Non" />
-            </RadioGroup>
-          </FormControl>
-          <FormControl fullWidth>
-            <FormLabel id="demo-row-radio-buttons-group-label" sx={{ mt: 1 }}>Accès à un jardin</FormLabel>
-            <RadioGroup
-              row
-              aria-labelledby="demo-row-radio-buttons-group-label"
-              name="row-radio-buttons-group"
-            >
-              <FormControlLabel value="female" control={<Radio />} label="Oui" />
-              <FormControlLabel value="male" control={<Radio />} label="Non" />
-            </RadioGroup>
-          </FormControl>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Lancer la recherche
+              <Typography>Rechercher un animal</Typography>
+            </AccordionSummary>
           </Button>
-        </Box>
+          <AccordionDetails>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label" sx={{ mt: 1 }}>Espèce</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={species}
+                  label="Species"
+                  onChange={handleChangeSpecies}
+                  sx={{ mt: 2 }}
+                >
+                  <MenuItem value={10}>Chien</MenuItem>
+                  <MenuItem value={20}>Chat</MenuItem>
+                  <MenuItem value={30}>Lapin</MenuItem>
+                  <MenuItem value={40}>Rongeur</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl fullWidth>
+                <FormLabel id="demo-row-radio-buttons-group-label" sx={{ mt: 1 }}>Genre</FormLabel>
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-row-radio-buttons-group-label"
+                  name="row-radio-buttons-group"
+                >
+                  <FormControlLabel value="femelle" control={<Radio />} label="Femelle" />
+                  <FormControlLabel value="mâle" control={<Radio />} label="Mâle" />
+                  <FormControlLabel value="both" control={<Radio />} label="Indifférent" />
+                </RadioGroup>
+              </FormControl>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label" sx={{ mt: 1 }}>Âge</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={age}
+                  label="Age"
+                  onChange={handleChangeAge}
+                  sx={{ mt: 2 }}
+                >
+                  <MenuItem value={10}>0 à 5 ans</MenuItem>
+                  <MenuItem value={20}>5 à 10 ans</MenuItem>
+                  <MenuItem value={30}>10 à 15 ans</MenuItem>
+                  <MenuItem value={40}>plus de 15 ans</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl fullWidth>
+                <FormLabel id="demo-row-radio-buttons-group-label" sx={{ mt: 1 }}>Compatibilité enfants</FormLabel>
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-row-radio-buttons-group-label"
+                  name="row-radio-buttons-group"
+                >
+                  <FormControlLabel value="female" control={<Radio />} label="Oui" />
+                  <FormControlLabel value="male" control={<Radio />} label="Non" />
+                </RadioGroup>
+              </FormControl>
+              <FormControl fullWidth>
+                <FormLabel id="demo-row-radio-buttons-group-label" sx={{ mt: 1 }}>Compatibilité autres animaux</FormLabel>
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-row-radio-buttons-group-label"
+                  name="row-radio-buttons-group"
+                >
+                  <FormControlLabel value="female" control={<Radio />} label="Oui" />
+                  <FormControlLabel value="male" control={<Radio />} label="Non" />
+                </RadioGroup>
+              </FormControl>
+              <FormControl fullWidth>
+                <FormLabel id="demo-row-radio-buttons-group-label" sx={{ mt: 1 }}>Accès à un jardin</FormLabel>
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-row-radio-buttons-group-label"
+                  name="row-radio-buttons-group"
+                >
+                  <FormControlLabel value="female" control={<Radio />} label="Oui" />
+                  <FormControlLabel value="male" control={<Radio />} label="Non" />
+                </RadioGroup>
+              </FormControl>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Lancer la recherche
+              </Button>
+            </Box>
+          </AccordionDetails>
+        </Accordion>
       </Box>
     </Container>
   );
