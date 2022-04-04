c901101f8469c346de67d85c70ca23e8d12be59a
Mathilde-J <mthldjanssen@gmail.com>
2022-04-04 15:26:59 +0200

    feat(components animalDescription AssoContact ContactButton):css mobile colors and padding fix

--- src/components/SearchedAnimals/AssociationContact/index.js
+++ src/components/SearchedAnimals/AssociationContact/index.js
@@ -1,9 +1,29 @@
+import './styles.scss';
+
 const AssociationContact = () => (
-  <div className="association-contact">
-    <p>nom de l'association</p>
-    <p>adrdresse de l'asso</p>
-    <p>téléphone</p>
-    <p>email</p>
+  <div className="association__infos">
+    <p
+      className="association__name"
+    >
+      nom de l'association
+    </p>
+
+    <p
+      className="association__address"
+    >
+      addresse de l'asso
+    </p>
+
+    <p
+      className="association__phone"
+    >
+      téléphone
+    </p>
+    <p
+      className="association__mail"
+    >
+      email
+    </p>
   </div>
 );
 
