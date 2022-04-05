4d3cf8430d75181a8f4d6c12446643cdc55a8726
Francois-Louis <fl.toussaint.art@gmail.com>
2022-04-05 12:42:48 +0200

    wip: assoList - css

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,6 @@
 // == Import
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import SearchForm from '../SearchForm';
+import AssoList from '../AssoList';
 import './styles.css';
 
 const theme = createTheme({
@@ -25,7 +25,7 @@ const theme = createTheme({
 const App = () => (
   <div className="app">
     <ThemeProvider theme={theme}>
-      <SearchForm />
+      <AssoList />
     </ThemeProvider>
   </div>
 );
---
+++ src/components/AssoList/index.js
@@ -0,0 +1,83 @@
+import {
+  MapContainer, TileLayer, Marker, Popup,
+} from 'react-leaflet';
+import './assoList.scss';
+
+const AssoList = () => {
+  return (
+    <div className="assoInfo">
+      <MapContainer center={[46.5, 2.6]} zoom={5}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+        </MapContainer>
+        <div className="assoInfo__list">
+          <div className="assoInfo__card">
+            <h3>DisneyLand</h3>
+            <p>Canards, souris et autres bêtes en tout genre, qui parlent !</p>
+            <ul className="assoInfo__contact">
+              <li>6 rue de la tourette</li>
+              <li>75000 Paris</li>
+              <li>08 36 65 65 65</li>
+              <li>vador@dark.fr</li>
+            </ul>
+          </div>
+          <div className="assoInfo__card">
+            <h3>DisneyLand</h3>
+            <p>Canards, souris et autres bêtes en tout genre, qui parlent !</p>
+            <ul className="assoInfo__contact">
+              <li>6 rue de la tourette</li>
+              <li>75000 Paris</li>
+              <li>08 36 65 65 65</li>
+              <li>vador@dark.fr</li>
+            </ul>
+          </div>
+          <div className="assoInfo__card">
+            <h3>DisneyLand</h3>
+            <p>Canards, souris et autres bêtes en tout genre, qui parlent !</p>
+            <ul className="assoInfo__contact">
+              <li>6 rue de la tourette</li>
+              <li>75000 Paris</li>
+              <li>08 36 65 65 65</li>
+              <li>vador@dark.fr</li>
+            </ul>
+          </div>
+          <div className="assoInfo__card">
+            <h3>DisneyLand</h3>
+            <p>Canards, souris et autres bêtes en tout genre, qui parlent !</p>
+            <ul className="assoInfo__contact">
+              <li>6 rue de la tourette</li>
+              <li>75000 Paris</li>
+              <li>08 36 65 65 65</li>
+              <li>vador@dark.fr</li>
+            </ul>
+          </div>
+          <div className="assoInfo__card">
+            <h3>DisneyLand</h3>
+            <p>Canards, souris et autres bêtes en tout genre, qui parlent !</p>
+            <ul className="assoInfo__contact">
+              <li>6 rue de la tourette</li>
+              <li>75000 Paris</li>
+              <li>08 36 65 65 65</li>
+              <li>vador@dark.fr</li>
+            </ul>
+          </div>
+          <div className="assoInfo__card">
+            <h3>DisneyLand</h3>
+            <p>Canards, souris et autres bêtes en tout genre, qui parlent !</p>
+            <ul className="assoInfo__contact">
+              <li>6 rue de la tourette</li>
+              <li>75000 Paris</li>
+              <li>08 36 65 65 65</li>
+              <li>vador@dark.fr</li>
+            </ul>
+          </div>
+        </div>
+    </div>
+        
+      
+  );
+};
+
+export default AssoList;
