7feaf296afe097ae0260d8d430fa244b756b3737
anthonymayor0clock <amayor@hotmail.fr>
2022-04-06 14:50:09 +0200

    making phone page qui sommes nous

--- src/components/Presentation/index.js
+++ src/components/Presentation/index.js
@@ -5,33 +5,43 @@ const Team = () => (
 
   <div className="box-container">
     <div className="box">
-      <img src='' alt='photoprofil'/>
+        <div className='box__info'>
+      <img className="picture" src='' alt='photoprofil'/>
       <h3 className="box__name">Julien</h3>
+        </div>
       <p className="box__text">Vivamus euismod lectus in feugiat tempor. Nunc pulvinar ante sed iaculis pulvinar. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer id laoreet urna. Nulla porta tristique lectus facilisis congue. Cras ut pharetra libero. Proin sed ullamcorper orci, eu tristique augue. Curabitur aliquet aliquet sollicitudin. Donec varius molestie magna, eu posuere massa porttitor et.</p>
     </div>
   
     <div className="box">
-      <img src='' alt='photoprofil'/>
+      <div className='box__info'>
+      <img className="picture" src='' alt='photoprofil'/>
       <h3 className="box__name">Virginie</h3>
+      </div>
       <p className="box__text">Vivamus euismod lectus in feugiat tempor. Nunc pulvinar ante sed iaculis pulvinar. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer id laoreet urna. Nulla porta tristique lectus facilisis congue. Cras ut pharetra libero. Proin sed ullamcorper orci, eu tristique augue. Curabitur aliquet aliquet sollicitudin. Donec varius molestie magna, eu posuere massa porttitor et.</p>
     </div>
 
     <div className="box">
-      <img src='' alt='photoprofil'/>
+      <div className='box__info'>
+      <img className="picture" src='' alt='photoprofil'/>
       <h3 className="box__name">François</h3>
+        </div>
       <p className="box__text">Vivamus euismod lectus in feugiat tempor. Nunc pulvinar ante sed iaculis pulvinar. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer id laoreet urna. Nulla porta tristique lectus facilisis congue. Cras ut pharetra libero. Proin sed ullamcorper orci, eu tristique augue. Curabitur aliquet aliquet sollicitudin. Donec varius molestie magna, eu posuere massa porttitor et.</p>
     </div>
 
 
     <div className="box">
-      <img src='' alt='photoprofil'/>
-      <h3 className="box__name">Mathilde</h3>
+      <div className='box__info'>
+        <img className="picture" src='' alt='photoprofil'/>
+        <h3 className="box__name">Mathilde</h3>
+      </div>
       <p className="box__text">Vivamus euismod lectus in feugiat tempor. Nunc pulvinar ante sed iaculis pulvinar. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer id laoreet urna. Nulla porta tristique lectus facilisis congue. Cras ut pharetra libero. Proin sed ullamcorper orci, eu tristique augue. Curabitur aliquet aliquet sollicitudin. Donec varius molestie magna, eu posuere massa porttitor et.</p>
     </div>
 
     <div className="box">
-      <img src='' alt='photoprofil'/>
-      <h3 className="box__name">Anthony</h3>
+      <div className='box__info'>
+        <img className="picture" src='' alt='photoprofil'/>
+        <h3 className="box__name">Anthony</h3>
+      </div>
       <p className="box__text">Vivamus euismod lectus in feugiat tempor. Nunc pulvinar ante sed iaculis pulvinar. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer id laoreet urna. Nulla porta tristique lectus facilisis congue. Cras ut pharetra libero. Proin sed ullamcorper orci, eu tristique augue. Curabitur aliquet aliquet sollicitudin. Donec varius molestie magna, eu posuere massa porttitor et.</p>
     </div>
 
