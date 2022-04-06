04cbcffd2d611a2e0b6dc9c897f1b14fc0112a0c
anthonymayor0clock <amayor@hotmail.fr>
2022-04-06 10:27:51 +0200

    add footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -13,6 +13,11 @@ const App = () => (
     <NavBar />
 <h1>Boule de poils</h1>
     <Presentation />
+    <div className='footer'>
+      <h3>Qui sommes-nous ?</h3>
+      <h4>Mentions Légales</h4>
+    </div>
+  
   </div>
 );
 
