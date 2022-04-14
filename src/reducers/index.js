a9d188833c2b282a974438069904c86f7488293e
Mathilde-J <mthldjanssen@gmail.com>
2022-04-14 14:01:51 +0200

    feat(SubscribeForm, registration reducers, register actions): check if mail ok, if false display an error message if true, proceed with the registration

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 
-
 import loginReducer from './login';
 import assoReducer from './assoReducer';
 import formReducer from './formReducer';
@@ -16,7 +15,6 @@ const rootReducer = combineReducers({
   FormReducer: formReducer,
   Login: loginReducer,
   AssoReducer: assoReducer,
-
   Carroussel: carrouselReducer,
   Association: associationReducer,
 });
