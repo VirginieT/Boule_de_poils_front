6521add5d66eea5eed9bb19680c96b649fc0b034
Francois-Louis <fl.toussaint.art@gmail.com>
2022-04-15 00:31:50 +0200

    fix: css error and void results

--- src/components/SearchedAnimals/index.js
+++ src/components/SearchedAnimals/index.js
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { fetchAnimals } from 'src/actions/animalSearched';
 import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import Loader from '../Loader';
 import Arrows from './Arrows';
@@ -17,6 +18,10 @@ const SearchedAnimals = () => {
   useEffect(() => {
     dispatch(fetchAnimals());
   }, []);
+  
+  function refreshPage() {
+    window.location.reload(false);
+  }
 
   const displayedProfile = useSelector((state) => state.SearchedAnimals.displayProfile);
 
@@ -44,8 +49,8 @@ const SearchedAnimals = () => {
     return (
       <div className="animal__profil">
         <CssBaseline />
-        <h1 className="annimal__void">Navrés pour vous, mais les animaux que vous recherchiez semblent avoir trouvé leur famille pour la vie !</h1>
-        <h3><Link to="/">Effectuer une nouvelle recherche</Link></h3>
+        <h2 className="animal__void">Navrés pour vous, mais les animaux que vous recherchiez semblent avoir trouvé leur famille pour la vie !</h2>
+        <Button variant="contained" sx={{ mb: '1em', widht: 'sm' }} onClick={refreshPage}><a href="/#searchForm">Effectuer une nouvelle recherche</a></Button>
         <img className="animal__picture" src="https://images.pexels.com/photos/1462636/pexels-photo-1462636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="animal's profile" />
       </div>
     );
@@ -55,7 +60,7 @@ const SearchedAnimals = () => {
       <div className="animal__profil">
         <CssBaseline />
         <h1 className="annimal__void">Oh non, votre requête semble avoir rencontré un pépin !</h1>
-        <h3><Link to="/">Effectuer une nouvelle recherche</Link></h3>
+        <Button variant="contained" sx={{ mb: '1em', widht: 'sm' }} onClick={refreshPage}><a href="/#searchForm">Effectuer une nouvelle recherche</a></Button>
         <img className="animal__picture" src="https://images.pexels.com/photos/127027/pexels-photo-127027.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="animal's profile" />
       </div>
     );
