f1fb1ebc4cf83c35fadddd18a78428f741252f86
Mathilde-J <mthldjanssen@gmail.com>
2022-04-15 10:18:46 +0200

    (Nabar component): fix merge added role

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom';
 import { logout } from 'src/actions/login';
 
 
-
 import './navbar.scss';
 
 import logo from 'src/assets/img/logo_bdp_dark.png';
 
 const NavBar = () => {
+  const role = useSelector((state) => (state.Login.role));
   const dispatch = useDispatch();
   const connectedUser = useSelector((state) => (state.Login.tokenUserConnected));
 
@@ -25,11 +25,11 @@ const NavBar = () => {
       <h1 className="navbar__title">Boules de poil</h1>
       <ul className="navbar__links">
         {connectedUser && (
-        <li className="navbar__item slideInDown-1 ">
-          <a href="/#searchForm" className="navbar__link">
-            Mes favoris
-          </a>
-        </li>
+          <li className="navbar__item slideInDown-1 ">
+            <a href="/#searchForm" className="navbar__link">
+              Mes favoris
+            </a>
+          </li>
         )}
         <li className="navbar__item slideInDown-2 ">
           <a href="/" className="navbar__link">
@@ -47,32 +47,32 @@ const NavBar = () => {
           </a>
         </li>
         {(role !== 'ROLE_USER' && role !== null) && (
-        <li className="navbar__item slideInDown-5">
-          <a href="/login" className="navbar__link">
-            Admin/compte user
-          </a>
-        </li>
+          <li className="navbar__item slideInDown-5">
+            <a href="/login" className="navbar__link">
+              Admin/compte user
+            </a>
+          </li>
         )}
         {connectedUser ? (
-            <li>
-              <Link
-                to="/"
-                className="items"
-                onClick={() => (dispatch(logout()))}
-              >
-                Logout
-              </Link>
-            </li>
-          ) : (
-            <li>
-              <Link
-                to="/login"
-                className="items"
-              >
-                Login
-              </Link>
-            </li>
-          )}
+          <li>
+            <Link
+              to="/"
+              className="items"
+              onClick={() => (dispatch(logout()))}
+            >
+              Logout
+            </Link>
+          </li>
+        ) : (
+          <li>
+            <Link
+              to="/login"
+              className="items"
+            >
+              Login
+            </Link>
+          </li>
+        )}
       </ul>
       <button type="button" className="navbar__burger" onClick={handleShowLinks}>
         <span className="burger-bar" />
