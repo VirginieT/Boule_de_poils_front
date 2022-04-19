d211bfecb843520aafa5af301970490f47828156
Mathilde-J <mthldjanssen@gmail.com>
2022-04-19 10:39:40 +0200

    fix field picture to imageName

--- src/components/SearchedAnimals/index.js
+++ src/components/SearchedAnimals/index.js
@@ -18,7 +18,7 @@ const SearchedAnimals = () => {
   useEffect(() => {
     dispatch(fetchAnimals());
   }, []);
-  
+
   function refreshPage() {
     window.location.reload(false);
   }
@@ -38,7 +38,7 @@ const SearchedAnimals = () => {
       <div className="animal__profil">
         <CssBaseline />
         <Arrows />
-        <AnimalPicture picture={animalProfile.picture} />
+        <AnimalPicture picture={animalProfile.imageName} />
         <AnimalDescription {...animalProfile} />
         <ContactButton showContactValue={showContact} />
         {showContact && (<AssociationContact {...animalProfile.association} />)}
