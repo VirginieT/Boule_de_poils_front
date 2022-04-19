fe9a904485befdfe7486e4e8ab7d12f8bde0cbe7
Mathilde-J <mthldjanssen@gmail.com>
2022-04-19 16:37:42 +0200

    site introduction

--- src/components/Presentation/index.js
+++ src/components/Presentation/index.js
@@ -24,12 +24,32 @@ const Presentation = () => {
     <div className="random">
       {allAnimalProfiles.length > 0 ? (
         <>
-          <div className="random__info">
-            <h2 className="random__name">{animalProfile.name}</h2>
-            <p className="random__description">{animalProfile.description}</p>
+          <div className="site__identity">
+            <h1 className="site__maintitle">Boule de poils</h1>
+            <p>Bienvenue sur le site Boule de poils,
+              ici, tu trouveras plein d'animaux qui n'attendent
+              qu'un foyer aimant pour les accueillir !
+              Effectue une recherche et tut auras peut-être
+              la chance de tomber sur le profil de :
+            </p>
           </div>
+
           <div className="random__carrousel">
             <Carrousel />
+          </div>
+
+          <div className="random__info">
+            <div className="site__identity--medium">
+              <h1 className="site__maintitle--medium">Boule de poils</h1>
+              <p>Bienvenue sur le site Boule de poils,
+                ici, tu trouveras plein d'animaux qui n'attendent
+                qu'un foyer aimant pour les accueillir !
+                Effectue une recherche et tut auras peut-être
+                la chance de tomber sur le profil de :
+              </p>
+            </div>
+            <h2 className="random__name">{animalProfile.name}</h2>
+            <p className="random__description">{animalProfile.description}</p>
           </div>
         </>
       ) : <Loader />}
