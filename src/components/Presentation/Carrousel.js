1786161082661caa1c0cda3959029ff63d9a134a
Mathilde-J <mthldjanssen@gmail.com>
2022-04-20 11:50:53 +0200

    fix(carroussel component) clear interval clear interval each time the carroussel is unmounted

--- src/components/Presentation/Carrousel.js
+++ src/components/Presentation/Carrousel.js
@@ -5,32 +5,33 @@ import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const Carrousel = () => {
-  useEffect(() => {
-    autoChange();
-  }, []);
-
   const dispatch = useDispatch();
+
+  const nextSlide = () => {
+    console.log('on execute cette fonction toutes les 5 secondes et quand on clique');
+    clearInterval();
+    dispatch(displayNextSlide());
+  };
+
+  const prevSlide = () => {
+    console.log('on execute cette fonction quand on clique');
+    clearInterval();
+    dispatch(displayPreviousSlide());
+  };
+
+  useEffect(() => {
+    const interval = setInterval(nextSlide, 5000);
+    return () => clearInterval(interval);
+  });
 
   const displayedProfile = useSelector((state) => state.Carroussel.displayProfile);
 
   const allAnimalProfiles = useSelector((state) => (state.Carroussel.apiresults));
 
-  const nextSlide = () => {
-    dispatch(displayNextSlide());
-  };
-
-  const prevSlide = () => {
-    dispatch(displayPreviousSlide());
-  };
-
-  const autoChange = () => {
-    setInterval(nextSlide, 5000);
-  };
-
   return (
     <section className="slider">
-      <ArrowBackIosIcon className="left-arrow" onClick={nextSlide} />
-      <ArrowForwardIosIcon className="right-arrow" onClick={prevSlide} />
+      <ArrowBackIosIcon className="left-arrow" onClick={prevSlide} />
+      <ArrowForwardIosIcon className="right-arrow" onClick={nextSlide} />
       {allAnimalProfiles.map((slide, index) => (
         <div
           className={index === displayedProfile ? 'slide active' : 'slide'}
--- src/middlewares/carrousselMiddleware.js
+++ src/middlewares/carrousselMiddleware.js
@@ -3,10 +3,10 @@ import axios from 'axios';
 import { CONNEXION_CARROUSSEL_API, saveData } from '../actions/carroussel';
 
 const carrousselMiddleware = (store) => (next) => (action) => {
-    switch(action.type){
-      case CONNEXION_CARROUSSEL_API:
-        axios.get('http://localhost:8081/api/animal/caroussel', {})
-        
+  switch (action.type) {
+    case CONNEXION_CARROUSSEL_API:
+      axios.get('http://localhost:8081/api/animal/caroussel', {})
+
         .then((response) => {
           // console.log(response.data);
           store.dispatch(saveData(response.data));
@@ -16,13 +16,11 @@ const carrousselMiddleware = (store) => (next) => (action) => {
           // handle error
           console.log(error);
         });
-         break;
-        default:
-      }
+      break;
+    default:
+  }
 
-      next(action);
-
+  next(action);
 };
 
 export default carrousselMiddleware;
-
