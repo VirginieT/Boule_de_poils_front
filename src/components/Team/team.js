6f2e8d78ac3db4c8909ff333fa1c4be342e4ec70
Francois-Louis <fl.toussaint.art@gmail.com>
2022-04-21 14:25:06 +0200

    feat(team): add links and techno

--- src/components/Team/team.js
+++ src/components/Team/team.js
@@ -2,6 +2,7 @@ import { Button } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import { FaSymfony, FaReact } from 'react-icons/fa';
 import AvatarF from 'src/assets/images/avatar_francois.png';
 import AvatarJ from 'src/assets/images/avatar_julien.png';
 import AvatarM from 'src/assets/images/avatar_mathilde.png';
@@ -16,42 +17,47 @@ const Team = () => (
     <div className="team__list">
       <div className="team__card">
         <img className="team__pic" src={AvatarJ} alt="Julien" />
+        <FaSymfony className="symfoLogo"/>
         <h3 className="team__name">Julien</h3>
         <h4 className="team__role">Product Owner / Lead Dev Back</h4>
         <div className="team__links">
-          <a href=""><LinkedInIcon className="team__ldkn" style={{ fontSize: 40 }}></LinkedInIcon></a>
-          <a href=""><GitHubIcon className="team__gh" style={{ fontSize: 40 }}></GitHubIcon></a>
+          <a href="https://www.linkedin.com/in/julienlaurent77/"><LinkedInIcon className="team__ldkn" style={{ fontSize: 40 }}></LinkedInIcon></a>
+          <a href="https://github.com/JulienLaurent77"><GitHubIcon className="team__gh" style={{ fontSize: 40 }}></GitHubIcon></a>
         </div>
       </div>
       <div className="team__card">
         <img className="team__pic" src={AvatarM} alt="Mathilde" />
+        <FaReact className="reactLogo"/>
         <h3 className="team__name">Mathilde</h3>
         <h4 className="team__role">Scrum Master</h4>
         <div className="team__links">
-          <a href=""><LinkedInIcon className="team__ldkn" style={{ fontSize: 40 }}></LinkedInIcon></a>
-          <a href=""><GitHubIcon className="team__gh" style={{ fontSize: 40 }}></GitHubIcon></a>
+          <a href="https://www.linkedin.com/in/mathildejanssenld/"><LinkedInIcon className="team__ldkn" style={{ fontSize: 40 }}></LinkedInIcon></a>
+          <a href="https://github.com/Mathilde-J"><GitHubIcon className="team__gh" style={{ fontSize: 40 }}></GitHubIcon></a>
         </div>
       </div>
       <div className="team__card">
         <img className="team__pic" src={AvatarA} alt="Anthony" />
+        <FaReact className="reactLogo"/>
         <h3 className="team__name">Anthony</h3>
         <h4 className="team__role">Lead Dev Front</h4>
         <div className="team__links">
-          <a href=""><LinkedInIcon className="team__ldkn" style={{ fontSize: 40 }}></LinkedInIcon></a>
-          <a href=""><GitHubIcon className="team__gh" style={{ fontSize: 40 }}></GitHubIcon></a>
+          <a href="https://www.linkedin.com/in/anthony-mayor-4350111b4/"><LinkedInIcon className="team__ldkn" style={{ fontSize: 40 }}></LinkedInIcon></a>
+          <a href="https://github.com/anthonymayor0clock"><GitHubIcon className="team__gh" style={{ fontSize: 40 }}></GitHubIcon></a>
         </div>
       </div>
       <div className="team__card">
         <img className="team__pic" src={AvatarV} alt="Virginie" />
+        <FaSymfony className="symfoLogo"/>
         <h3 className="team__name">Virginie</h3>
         <h4 className="team__role">Git Master</h4>
         <div className="team__links">
-          <a href=""><LinkedInIcon className="team__ldkn" style={{ fontSize: 40 }}></LinkedInIcon></a>
-          <a href=""><GitHubIcon className="team__gh" style={{ fontSize: 40 }}></GitHubIcon></a>
+          <a href="https://www.linkedin.com/in/virginie-touzalin-13ba54226/"><LinkedInIcon className="team__ldkn" style={{ fontSize: 40 }}></LinkedInIcon></a>
+          <a href="https://github.com/VirginieT"><GitHubIcon className="team__gh" style={{ fontSize: 40 }}></GitHubIcon></a>
         </div>
       </div>
       <div className="team__card">
-        <img className="team__pic" src={AvatarF} alt="Julien" />
+        <img className="team__pic" src={AvatarF} alt="François-Louis" />
+        <FaReact className="reactLogo"/>
         <h3 className="team__name">François-Louis</h3>
         <h4 className="team__role">Technical Advisor</h4>
         <div className="team__links">
