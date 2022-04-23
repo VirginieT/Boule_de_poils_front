38bee3a5d1618f6e94cd7d3b8ad2a14cfa7906d9
Francois-Louis <fl.toussaint.art@gmail.com>
2022-04-23 20:03:48 +0200

    wip: trying more tests on advices component

--- tests/components/Advices.test.js
+++ tests/components/Advices.test.js
@@ -8,4 +8,20 @@ describe('<Advices />', () => {
     const h2Elements = wrapper.find('h2');
     expect(h2Elements).toHaveLength(1);
   });
+  test('has 5 subtitles', () => {
+    const wrapper = shallow(<Advices />);
+    const h3Elements = wrapper.find('h3');
+    expect(h3Elements).toHaveLength(5);
+  });
+  test('has 5 paragraphs', () => {
+    const wrapper = shallow(<Advices />);
+    const pElements = wrapper.find('p');
+    expect(pElements).toHaveLength(5);
+  });
+  test('has 3 images', () => {
+    const wrapper = shallow(<Advices />);
+    const imgElements = wrapper.find('img');
+    expect(imgElements).toHaveLength(3);
+  });
 });
+
